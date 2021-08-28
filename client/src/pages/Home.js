5ab46d2964a494db0bfc012cdc4707a7a51406ce
Brian W <brianwalters952@gmail.com>
2021-08-28 13:20:07 -0500

    created Home page, and updated App.js to display Home view

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+      <div>
+        <h1>Title Placeholder</h1>
+        <button id='startButton'>Quick Play</button>
+    </div>
+  );
+};
+
+export default Home;
