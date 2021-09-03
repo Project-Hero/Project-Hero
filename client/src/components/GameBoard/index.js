223ebb81f3116650446023cc7dd39dd9c94917f7
bluninjadragon <bluninjadragon@gmail.com>
2021-09-02 20:53:46 -0500

    added logic to loop blank answers through card. added onkeypress

--- client/src/components/GameBoard/index.js
+++ client/src/components/GameBoard/index.js
@@ -3,26 +3,41 @@ import { useQuery } from "@apollo/client";
 import { QUERY_QUESTIONS } from "../../utils/queries";
 
 export default function GameBoard() {
-
-  const [phrase, setPhrase] = useState([])
-
+  const [phrase, setPhrase] = useState([]);
+  const [blanksLetters, setBlankLetters] = useState([]);
 
   const { loading, data } = useQuery(QUERY_QUESTIONS);
   const questions = data?.questions || [];
-  const [hidden, setHidden] = useState({})
-  const handleAnswer = (index) => {
+  const [hidden, setHidden] = useState({});
+  const handleBlanks = (index) => {
     setPhrase(questions[index].phrase.split(""));
+
+    let numBlanks = phrase.length;
+    let blanksLetters = [];
+    // Uses loop to push blanks to blankLetters array
+    for (var i = 0; i < numBlanks; i++) {
+      blanksLetters.push("_  ");
+    }
+    setBlankLetters(blanksLetters);
+    // Converts blankLetters array into a string and renders it on the screen
+    console.log(blanksLetters);
     console.log(phrase);
     setHidden({ ...hidden, [index]: !hidden[index] });
-
-  }
+  };
+  const handleAnswer = (event) => {
+    if (event.key === "y") {
+      console.log("The sky is your starting point!");
+    } else if (event.key === "n") {
+      console.log("The sky is your limit👀");
+    }
+  };
   return (
-    <div className="d-f">
+    <div className="d-f" onKeyPress={(e) => handleAnswer}>
       {" "}
       {questions &&
         questions.map((question, index) => (
           <div
-            onClick={(e) => handleAnswer(index)}
+            onClick={(e) => handleBlanks(index)}
             key={question._id}
             className="col-12 col-xl"
           >
@@ -34,7 +49,7 @@ export default function GameBoard() {
                 ${question.value} <br />
               </h4>
             </div>
-            <h4>{!hidden[index] && phrase}</h4>
+            <h4>{!hidden[index] && blanksLetters}</h4>
           </div>
         ))}
     </div>
