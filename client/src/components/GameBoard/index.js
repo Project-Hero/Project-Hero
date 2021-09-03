3cb526666029009bb149c35a2f6c4281f4c0b329
Brian W <brianwalters952@gmail.com>
2021-09-02 19:26:02 -0500

    worked on GameBoard component, getting answer to show on click & convert to array

--- client/src/components/GameBoard/index.js
+++ client/src/components/GameBoard/index.js
@@ -4,22 +4,29 @@ import { QUERY_QUESTIONS } from "../../utils/queries";
 
 export default function GameBoard() {
 
+  const [phrase, setPhrase] = useState([])
+
+
   const { loading, data } = useQuery(QUERY_QUESTIONS);
   const questions = data?.questions || [];
-  const handleAnswer = () => {
-    console.log(data);
+  const [hidden, setHidden] = useState({})
+  const handleAnswer = (index) => {
+    setPhrase(questions[index].phrase.split(""));
+    console.log(phrase);
+    setHidden({ ...hidden, [index]: !hidden[index] });
+
   }
   return (
     <div className="d-f">
       {" "}
       {questions &&
-        questions.map((question) => (
+        questions.map((question, index) => (
           <div
-            onClick={handleAnswer}
+            onClick={(e) => handleAnswer(index)}
             key={question._id}
             className="col-12 col-xl"
           >
-            <div className="card mb-3">
+            <div className="card mb-4">
               <h4 className="card-header bg-dark text-light p-2 m-0 center">
                 {question.category} <br />
               </h4>
@@ -27,6 +34,7 @@ export default function GameBoard() {
                 ${question.value} <br />
               </h4>
             </div>
+            <h4>{!hidden[index] && phrase}</h4>
           </div>
         ))}
     </div>
