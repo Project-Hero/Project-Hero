0d04f2fe55cf9cab6320dd10b69751fc7e5ca905
Brian W <brianwalters952@gmail.com>
2021-09-02 21:18:57 -0500

    fixed card double click issue, got event listener for keystrokes working kind of

--- client/src/components/GameBoard/index.js
+++ client/src/components/GameBoard/index.js
@@ -1,30 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import { QUERY_QUESTIONS } from "../../utils/queries";
 
 export default function GameBoard() {
   const [phrase, setPhrase] = useState([]);
   const [blanksLetters, setBlankLetters] = useState([]);
-
   const { loading, data } = useQuery(QUERY_QUESTIONS);
   const questions = data?.questions || [];
   const [hidden, setHidden] = useState({});
   const handleBlanks = (index) => {
     setPhrase(questions[index].phrase.split(""));
 
-    let numBlanks = phrase.length;
-    let blanksLetters = [];
-    // Uses loop to push blanks to blankLetters array
-    for (var i = 0; i < numBlanks; i++) {
-      blanksLetters.push("_  ");
-    }
-    setBlankLetters(blanksLetters);
-    // Converts blankLetters array into a string and renders it on the screen
-    console.log(blanksLetters);
-    console.log(phrase);
+
+
     setHidden({ ...hidden, [index]: !hidden[index] });
   };
+
+  useEffect(() => {
+   let numBlanks = phrase.length;
+   let blanksLetters = [];
+   // Uses loop to push blanks to blankLetters array
+   for (var i = 0; i < numBlanks; i++) {
+     blanksLetters.push("_  ");
+   }
+   setBlankLetters(blanksLetters);
+   // Converts blankLetters array into a string and renders it on the screen
+   console.log(blanksLetters);
+  }, [phrase])
+
+
+
   const handleAnswer = (event) => {
+    console.log('event', event);
+    console.log('event.key', event.key);
     if (event.key === "y") {
       console.log("The sky is your starting point!");
     } else if (event.key === "n") {
@@ -32,8 +40,7 @@ export default function GameBoard() {
     }
   };
   return (
-    <div className="d-f" onKeyPress={(e) => handleAnswer}>
-      {" "}
+    <div tabIndex="0" className="d-f" onKeyDown={handleAnswer}>
       {questions &&
         questions.map((question, index) => (
           <div
